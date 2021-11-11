42745490a5cd282a1add5d0f91d709975fbed6ca
Nemo Zhang <zhangguoxi1166@gmail.com>
2021-11-11 23:23:03 +0800

    update data

--- daily.js
+++ daily.js
@@ -1,6 +1,6 @@
 var now = new Date();
 
-var imgsrc = 'https://cdn.jsdelivr.net/gh/arknightscommunity/widget@2.0.20211111/res/';
+var reposrc = 'https://cdn.jsdelivr.net/gh/arknightscommunity/widget@main/';
 
 var json_src = 'https://arknightscommunity.drblack-system.com/Extend/daily-msg/'
 
@@ -9,12 +9,12 @@ jQuery(document).ready(function ($) {
         $("#daily").append("<div id='d_r'></div>");
         $("#d_r").append("<div class='d-item d-r' id='d_res'>物资筹备：</div>");
         daily_res[0].forEach(item => {
-            src = imgsrc+item.src
+            src = reposrc+'res/'+item.src
             $("#d_res").append('<img src='+src+' title='+item.name+' alt='+item.name+' class="d-res">')
         });
         $("#d_r").append("<div class='d-item d-r' id='d_chip'>芯片搜索：</div>");
         daily_res[1].forEach(item => {
-            src = imgsrc+item.src
+            src = reposrc+'res/'+item.src
             $("#d_chip").append('<img src='+src+' title='+item.name+' alt='+item.name+' class="d-chip">')
         });
     });
@@ -25,7 +25,8 @@ jQuery(document).ready(function ($) {
             $("#d_o").append('今天是干员<span class="op-bir">'+ops.join('、')+'</span>的生日。')
         }
     })
-    $.getJSON('https://cdn.jsdelivr.net/gh/arknightscommunity/widget@latest/activity.json',function (activity) {
+    $.getJSON(reposrc+'activity.json',function (data) {
+        activity = data[0]
         if (activity.length==0) { } else {
             $("#daily").append("<div id='d_a'></div>");
             var idx=0;
